d70c3a1a050eff0ccb85d76590b2296bbf17fdba
dmfanua <h.mudrytskyi@gmail.com>
2024-03-06 15:16:06 +0200

    deleted:    README.pl.md 	deleted:    README.ro.md 	deleted:    README.ru.md 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/page-3.html -> src/1-gallery.html 	new file:   src/2-form.html 	modified:   src/css/styles.css 	modified:   src/index.html 	new file:   src/js/1-gallery.js 	new file:   src/js/2-form.js 	renamed:    src/main.js -> src/js/main.js 	deleted:    src/page-2.html 	modified:   src/partials/header.html

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', saveToLocalStorage);
+
+emailInput.value = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state')).email
+  : '';
+messageInput.value = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state')).message
+  : '';
+
+form.addEventListener('submit', handleSubmit);
+
+function saveToLocalStorage() {
+  const formData = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
